25d5dbbb5609300e6e6f413bcb4b558526070c94
silverweed <silverweed1991@gmail.com>
2016-10-16 12:58:29 +0200

    update runcppclean.sh

--- runcppclean.sh
+++ runcppclean.sh
@@ -1,1 +1,2 @@
-cppclean -I src/core -I src/lifish/ -I src/win/ -I src/ui/ -I src/third_party/ -I src/lifish/entities/ -I src/lifish/components/ -I src/lifish/level/ --exclude src/old/ src/ > clean.out
+cppclean -I src/core -I src/lifish/ -I src/win/ -I src/ui/ -I src/third_party/ -I src/lifish/entities/ -I src/lifish/components/ -I src/lifish/level/ --exclude src/old/ src/ | grep -v old > clean.out
+>&2 echo "Output written to clean.out"
