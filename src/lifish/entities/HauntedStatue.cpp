9544a7a0dc57bc591aebbe1a2e856f8487f62ca7
silverweed <silverweed1991@gmail.com>
2016-12-15 09:00:20 +0100

    fix HauntedStatue origin

--- src/lifish/entities/HauntedStatue.cpp
+++ src/lifish/entities/HauntedStatue.cpp
@@ -15,8 +15,9 @@ HauntedStatue::HauntedStatue(const sf::Vector2f& pos) : Game::Entity(pos) {
 				*addComponent(new Game::Sprite(*this, Game::getAsset("graphics", "haunted_statue.png"),
 				sf::IntRect(0, 0, Game::TILE_SIZE, 2 * Game::TILE_SIZE)))));
 	addComponent(new Game::Collider(*this, Game::Layers::UNBREAKABLES, 
-				sf::Vector2i(Game::TILE_SIZE, Game::TILE_SIZE), 
-				sf::Vector2f(0, Game::TILE_SIZE)));
+				sf::Vector2i(Game::TILE_SIZE, Game::TILE_SIZE)));
 	addComponent(new Game::ZIndexed(*this, Game::Conf::ZIndex::BOSS_EXPLOSIONS));
 	addComponent(new Game::Fixed(*this));
+
+	get<Sprite>()->setOrigin(sf::Vector2f(0, 32));
 }
