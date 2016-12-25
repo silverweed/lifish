6bfa1d9b6e71f43c6c1b347143ce234c3b0da63a
silverweed <silverweed1991@gmail.com>
2016-12-25 12:26:08 +0100

    remove debug prjnt

--- src/lifish/entities/FreeBullet.cpp
+++ src/lifish/entities/FreeBullet.cpp
@@ -9,8 +9,6 @@
 #include "Animated.hpp"
 #include <cmath>
 
-#include <iostream>
-
 using Game::FreeBullet;
 using Game::TILE_SIZE;
 
@@ -21,9 +19,8 @@ FreeBullet::FreeBullet(const sf::Vector2f& pos, const Game::Entity *const source
 	FreeBulletPresets::setup(*this, attack.id);
 
 	addComponent(new Game::FreeMoving(*this, attack.speed, sf::Vector2f(std::cos(angle), std::sin(angle))));
-	collider = addComponent(new Game::Collider(*this, [this] (Game::Collider& cld) {
+	collider = addComponent(new Game::Collider(*this, [this] (Game::Collider&) {
 		// on collision
-		std::cerr << "colliding with " << cld.getLayer() << std::endl;
 		auto klb = get<Game::Killable>();
 		if (!klb->isKilled()) {
 			klb->kill();
