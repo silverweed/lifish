c21fca384349c0d7f67b86ee99e401b09163d457
silverweed <silverweed1991@gmail.com>
2017-01-07 13:46:52 +0100

    fix names in dialogs.hpp

--- src/core/dialogs.hpp
+++ src/core/dialogs.hpp
@@ -19,7 +19,7 @@ namespace lif {
  */
 std::string display_load_dialog() {
 #if defined(SFML_SYSTEM_WINDOWS)
-	return lif::Dialog::openFile();
+	return lif::dialog::openFile();
 #elif !defined(HAVE_NFD)
 	std::cout << "[ WARNING ] lifish was compiled without GTK support:\n"
 		     "if you want to load a game, place a file named `save.lifish`\n"
@@ -50,7 +50,7 @@ std::string display_load_dialog() {
  */
 std::string display_save_dialog() {
 #if defined(SFML_SYSTEM_WINDOWS)
-	return lif::Dialog::saveFile();
+	return lif::dialog::saveFile();
 #elif !defined(HAVE_NFD)
 	std::cout << "[ WARNING ] lifish was compiled without GTK support:\n"
 		     "the game will be saved in `" << lif::pwd << lif::DIRSEP << "save.lifish`."
