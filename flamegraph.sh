a82190880bd94a06bd2ddad2edd5ca8b993fd1fc
silverweed <silverweed1991@gmail.com>
2017-02-11 11:05:00 +0100

    add flamegraph.sh

---
+++ flamegraph.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -r F=/usr/local/src/FlameGraph
+set -x
+./lifish &>/dev/null&
+sudo perf record -F 80 -a -g -p $! -- sleep 60
+sudo perf script > /tmp/out.perf
+$F/stackcollapse-perf.pl /tmp/out.perf > /tmp/out.folded
+$F/flamegraph.pl /tmp/out.folded > /tmp/out.svg
+firefox /tmp/out.svg&
+pkill lifish
