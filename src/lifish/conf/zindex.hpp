871825a408e2779bbbfec7e7fcca584ce5794b21
silverweed <silverweed1991@gmail.com>
2017-08-02 12:27:59 +0200

    update zindexes

--- src/lifish/conf/zindex.hpp
+++ src/lifish/conf/zindex.hpp
@@ -5,19 +5,20 @@ namespace lif {
 namespace conf {
 	namespace zindex {
 		// Default layer is 0
-		constexpr int EXPLOSIONS      = 1;
-		constexpr int TRANSP_WALLS    = 2;
-		constexpr int WALLS           = 3;
-		constexpr int PONDS           = 4;
-		constexpr int PLAYERS         = 5;
-		constexpr int ENEMIES         = 6;
-		constexpr int TALL_ENTITIES   = 7;
-		constexpr int TORCHES         = 7;
-		constexpr int BULLETS         = 8;
-		constexpr int BOSSES          = 8;
-		constexpr int FOG             = 9;
-		constexpr int BOSS_EXPLOSIONS = 10;
-		constexpr int FLASHES         = 10;
+		constexpr int PONDS           = 1;
+		constexpr int EXPLOSIONS      = 2;
+		constexpr int TRANSP_WALLS    = 3;
+		constexpr int WALLS           = 4;
+		constexpr int BOMBS           = 5;
+		constexpr int PLAYERS         = 6;
+		constexpr int ENEMIES         = 7;
+		constexpr int TALL_ENTITIES   = 8;
+		constexpr int TORCHES         = 8;
+		constexpr int BULLETS         = 9;
+		constexpr int BOSSES          = 9;
+		constexpr int FOG             = 10;
+		constexpr int BOSS_EXPLOSIONS = 20;
+		constexpr int FLASHES         = 20;
 		constexpr int POINTS          = 42;
 		constexpr int DROPPING_TEXTS  = 42;
 	}
