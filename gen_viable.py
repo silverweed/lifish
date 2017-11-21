c2b4e081bcc24776a70163eef8523274e53d8fb6
silverweed <silverweed1991@gmail.com>
2017-11-21 11:34:52 +0100

    document gen_viable

--- gen_viable.py
+++ gen_viable.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 # Utility to generate valid positions for GodEyeBoss
+# Give a tilemap as first and only command line argument
+# Output is in stdout
 
 import sys
 
@@ -13,9 +15,9 @@ width = 15
 i = 0
 prev_line = 1
 for ch in tilemap:
-	if ch not in {'1', '2', '3', '+'}:
+	if ch not in '123+':
 		line = i // width + 1
 		print('{}{{{},{}}}'.format(' ' if line == prev_line else '\n', i % width + 1, line), end=',')
 		prev_line = line
 	i += 1
-print('')
+print()
