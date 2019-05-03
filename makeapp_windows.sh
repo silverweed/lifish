0b7afac823caafc878fce002c45fc958c37fafc7
silverweed <silverweed1991@gmail.com>
2019-05-03 13:58:03 +0200

    add makeapp_windows

---
+++ makeapp_windows.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+	echo "Usage: $0 <version>"
+	exit 1
+fi
+
+DIR="lifish_BOOM_${1}_windows_x64"
+
+mkdir -p "$DIR"
+if [[ -n $(ls "$DIR") ]]; then
+	echo "Directory $DIR not empty!"
+	exit 2
+fi
+
+[[ -d assets ]] || {
+	echo Missing assets directory!
+	exit 3
+}
+[[ -r levels.json ]] || {
+	echo Missing levels.json!
+	exit 3
+}
+[[ -x lifish ]] || {
+	echo Missing lifish executable! Did you compile the game?
+	exit 3
+}
+
+cp -r assets $DIR
+cp levels.json $DIR
+cp lifish $DIR
+cp windows/libs/* $DIR
+
+pushd "$DIR"
+
+cat > README.txt <<'EOF'
+------------------------------------
+BOOM Remake by silverweed
+https://github.com/silverweed/lifish
+------------------------------------
+
+The game will only work on a 64bit Windows.
+EOF
+
+popd
+
+echo "Created app in $DIR."
+echo Compressing...
+
+rm -f "$DIR.tar.xz" 2>/dev/null
+tar cf "$DIR.tar" "$DIR"
+xz "$DIR.tar"
+
+rm -rf "$DIR"
+echo Done.
