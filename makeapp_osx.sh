95be9522d3b48f3da9a4e4965774387cc27817af
silverweed <silverweed1991@gmail.com>
2022-09-10 12:47:03 +0200

    bump minor to 6, some cleanups
    - removed all duplicate ogg files from graphics
    - make zip preserve symlinks in makeapp_osx. This halves the size of the
    .app

--- makeapp_osx.sh
+++ makeapp_osx.sh
@@ -90,4 +90,4 @@ popd
 find "$APPNAME.app" -type f -exec install_name_tool -add_rpath "@executable_path/../Frameworks" {} \; 2>&1 | grep -v "not a Mach-O"
 
 rm -f "$OUT_ZIP" 2>/dev/null
-zip -r "$OUT_ZIP" "$APPNAME.app"
+zip -r --symlinks "$OUT_ZIP" "$APPNAME.app"
