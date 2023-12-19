a051444d45bec7e1c97f958d16da27a50b26307b
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2023-12-19 18:58:46 +0100

    Make the build script more streamlined and fully automated
      - Download, patch and compile SFML as universal binary with HID devices
        detection disabled to avoid spurious input monitoring prompts.
    
      - Compile BOOM Remake and bundle everything in an .app with correct
        location of assets (Resources folder), running as universal binary,
        with version in Info.plist and SFML framework correctly linked.
    
      - Package the app into a DMG as it is the customary way of distribution
        on macOS.

--- makeapp_osx.sh
+++ makeapp_osx.sh
@@ -1,93 +1,44 @@
-#!/bin/bash
-# Create a Mac OSX app bundle.
-# MUST be launched from the lifish directory
-# The lifish executable MUST already exist.
+#!/usr/bin/env bash
 
-if [[ $# < 1 ]]; then
-	echo "Usage: $0 <version>"
-	exit 1
+set -xe
+
+sfml_version="2.6.1"
+sfml_filename="SFML-$sfml_version-sources.zip"
+sfml_url="https://www.sfml-dev.org/files/$sfml_filename"
+
+if [ ! -f "$sfml_filename" ]; then
+	curl -sSf -L -O "$sfml_url"
+	echo "5bf19e5c303516987f7f54d4ff1b208a0f9352ffa1cd55f992527016de0e8cb7  $sfml_filename" | shasum -a 256 -c
+	unzip "$sfml_filename"
+	mv "SFML-$sfml_version" SFML
 fi
 
-OUT_ZIP="lifish_BOOM_${1}_osx.zip"
-APPNAME="BOOM Remake"
-MACOS=(lifish)
-RESOURCES=(levels.json)
-ASSETS=(assets/fonts assets/graphics assets/sounds assets/screens assets/music assets/l10n)
-FRAMEWORK_PATH=/Library/Frameworks
-FRAMEWORKS=({FLAC,ogg,freetype,OpenAL,vorbis{,enc,file}}.framework)
-# Ugh...
-LOCAL_FRAMEWORK_PATH="../SFML-2.5.1-macos-clang/Frameworks"
-LOCAL_FRAMEWORKS=(sfml-{window,graphics,system,audio}.framework)
-
-for i in ${MACOS[@]} ${RESOURCES[@]};  do
-	[[ -e $i ]] || {
-		echo "$i not found in this directory." >&2
-		exit 1
-	}
-done
-
-for i in ${FRAMEWORKS[@]}; do
-	[[ -d "$FRAMEWORK_PATH/$i" ]] || {
-		echo "$i not found in $FRAMEWORK_PATH". >&2
-		exit 1
-	}
-done
-
-# Lazy workaround to SFML not being *really* installed on my machine...
-for i in ${LOCAL_FRAMEWORKS[@]}; do
-	[[ -d "$LOCAL_FRAMEWORK_PATH/$i" ]] || {
-		echo "$i not found in $LOCAL_FRAMEWORK_PATH". >&2
-		exit 1
-	}
-done
-
-set -x
-
-rm -rf "$APPNAME.app"
-mkdir -p "$APPNAME.app"/Contents/{MacOS,Resources,Frameworks}
-for i in ${FRAMEWORKS[@]}; do
-	cp -R "$FRAMEWORK_PATH/$i" "$APPNAME.app"/Contents/Frameworks/.
-done
-for i in ${LOCAL_FRAMEWORKS[@]}; do
-	cp -R "$LOCAL_FRAMEWORK_PATH/$i" "$APPNAME.app"/Contents/Frameworks/.
-done
-
-cat > "$APPNAME.app"/Contents/Info.plist <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-    <dict>
-        <key>CFBundleExecutable</key>
-        <string>lifish</string>
-        <key>CFBundleIdentifier</key>
-        <string>github.com/silverweed/lifish</string>
-        <key>CFBundleInfoDictionaryVersion</key>
-        <string>0.0</string>
-        <key>CFBundleName</key>
-        <string>lifish</string>
-        <key>CFBundleDisplayName</key>
-        <string>BOOM Remake</string>
-        <key>CFBundlePackageType</key>
-        <string>APPL</string>
-	<key>CFBundleIconFile</key>
-	<string>BOOM.icns</string>
-    </dict>
-</plist>
-EOF
-
-cp -r ${MACOS[@]} "$APPNAME.app"/Contents/MacOS/.
-cp -r ${RESOURCES[@]} "$APPNAME.app"/Contents/Resources/.
-mkdir "$APPNAME.app"/Contents/Resources/{assets,saves}
-cp -r ${ASSETS[@]} "$APPNAME.app"/Contents/Resources/assets/
-cp osx/BOOM.icns "$APPNAME.app"/Contents/Resources/.
-
-pushd "$APPNAME.app"/Contents/MacOS
-ln -s ../Resources/assets
-ln -s ../Resources/saves
-ln -s ../Resources/levels.json
-
+pushd SFML
+# These arguments to patch avoid prompting to apply the changes repeatedly, making the script somewhat idempotent
+patch -N -r- -p 1 < ../osx/skip_obtaining_hid_devices.patch || true
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX=install -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DSFML_BUILD_FRAMEWORKS=TRUE
+cmake --build build --config Release --target install -j "$(sysctl -n hw.logicalcpu)"
 popd
-find "$APPNAME.app" -type f -exec install_name_tool -add_rpath "@executable_path/../Frameworks" {} \; 2>&1 | grep -v "not a Mach-O"
 
-rm -f "$OUT_ZIP" 2>/dev/null
-zip -r --symlinks "$OUT_ZIP" "$APPNAME.app"
+cmake -S . -B build -DRELEASE=true
+cmake --build build -j "$(sysctl -n hw.logicalcpu)"
+
+sw_version="1.6"
+
+mkdir -p "BOOM Remake.app"/Contents/{MacOS,Resources,Frameworks}
+
+cp -a osx/Info.plist "BOOM Remake.app/Contents/"
+cp -a build/lifish "BOOM Remake.app/Contents/MacOS/"
+cp -a osx/BOOM.icns "BOOM Remake.app/Contents/Resources/"
+cp -a assets/* "BOOM Remake.app/Contents/Resources/"
+cp -a levels.json "BOOM Remake.app/Contents/Resources/"
+cp -a SFML/install/sfml-{audio,graphics,system,window}.framework "BOOM Remake.app/Contents/Frameworks/"
+cp -a SFML/install/lib/{FLAC,OpenAL,freetype,ogg,vorbis,vorbisenc,vorbisfile}.framework "BOOM Remake.app/Contents/Frameworks/"
+
+sed -i '' "s/CHANGEME_SW_VERSION/$sw_version/" "BOOM Remake.app/Contents/Info.plist"
+
+echo -n "APPL????" > "BOOM Remake.app/Contents/PkgInfo"
+
+hdiutil create -fs HFS+ -srcfolder "BOOM Remake.app" -volname "BOOM Remake $sw_version" "BOOM Remake-$sw_version-mac.dmg"
+
+rm -r "BOOM Remake.app"
