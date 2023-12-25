d53498d32022de56c786c6ddb2112463687a0f0a
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2023-12-25 21:02:44 +0100

    assetDir already works on all OSes

--- src/core/Track.cpp
+++ src/core/Track.cpp
@@ -4,11 +4,7 @@
 lif::Track lif::getNthTrack(int num, float start, float length) {
 	using lif::DIRSEP;
 	std::stringstream ss;
-#if defined(SFML_SYSTEM_MACOS)
 	ss << lif::assetDir << "music" << DIRSEP << "music" << num << ".ogg";
-#else
-	ss << lif::pwd << DIRSEP << "assets" << DIRSEP << "music" << DIRSEP << "music" << num << ".ogg";
-#endif
 	lif::Track track;
 	track.name = ss.str();
 	track.loopstart = start;
