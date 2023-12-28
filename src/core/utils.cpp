a106122e7c478cbc576044bd92c3203950e91674
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2023-12-28 23:16:55 +0100

    Fix value of levelSet key in saved game

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -42,7 +42,11 @@ void lif::testMusic() {
 }
 
 std::string lif::toRelativePath(const std::string& path) {
+#if defined(SFML_SYSTEM_MACOS)
+	auto idx = lif::assetDir.length();
+#else
 	auto idx = strnlen(lif::pwd, lif::PWD_BUFSIZE);
+#endif
 
 	if (idx >= path.length()) return path;
 
