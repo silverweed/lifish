d0302c26351eb964a9d691462412434fbe9d6e81
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2024-01-07 09:40:07 +0100

    Default to Command keys on macOS to place a bomb

--- src/lifish/controls.cpp
+++ src/lifish/controls.cpp
@@ -7,14 +7,22 @@ std::array<sf::Keyboard::Key, lif::controls::CONTROLS_NUM> lif::controls::player
 		/* Control::DOWN, */ sf::Keyboard::Key::Down,
 		/* Control::LEFT, */ sf::Keyboard::Key::Left,
 		/* Control::RIGHT,*/ sf::Keyboard::Key::Right,
+#if defined(SFML_SYSTEM_MACOS)
+		/* Control::BOMB, */ sf::Keyboard::Key::RSystem
+#else
 		/* Control::BOMB, */ sf::Keyboard::Key::RControl
+#endif
 	}},
 	{{
 		/* Control::UP,    */ sf::Keyboard::Key::W,
 		/* Control::DOWN,  */ sf::Keyboard::Key::S,
 		/* Control::LEFT,  */ sf::Keyboard::Key::A,
 		/* Control::RIGHT, */ sf::Keyboard::Key::D,
+#if defined(SFML_SYSTEM_MACOS)
+		/* Control::BOMB,  */ sf::Keyboard::Key::LSystem
+#else
 		/* Control::BOMB,  */ sf::Keyboard::Key::LControl
+#endif
 	}}
 };
 
