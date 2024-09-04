67428ec9ea7e14f3ddb450fc18651901b3a96444
Orazio <22700499+orazioedoardo@users.noreply.github.com>
2024-09-04 11:27:25 +0200

    Add AppImage build tooling

--- makeapp_linux.sh
+++ makeapp_linux.sh
@@ -1,73 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ $# < 1 ]]; then
-	echo "Usage: $0 <version>"
-	exit 1
-fi
+set -xe
 
-DIR="lifish_BOOM_${1}_linux_x64"
+system_arch="$(uname -m)"
 
-mkdir -p "$DIR"
-if [[ -n $(ls "$DIR") ]]; then
-	echo "Directory $DIR not empty!"
-	exit 2
-fi
+sw_version="1.7"
 
-[[ -d assets ]] || {
-	echo Missing assets directory!
-	exit 3
-}
-[[ -r levels.json ]] || {
-	echo Missing levels.json!
-	exit 3
-}
-[[ -x lifish ]] || {
-	echo Missing lifish executable! Did you compile the game?
-	exit 3
-}
+appimagetool_version="13"
+appimagetool_filename="appimagetool-$system_arch.AppImage"
+appimagetool_url="https://github.com/AppImage/AppImageKit/releases/download/$appimagetool_version/$appimagetool_filename"
 
-mkdir "$DIR"/{assets,saves}
-cp -r assets/{fonts,graphics,l10n,music,screens,sounds} "$DIR/assets"
-cp levels.json $DIR
-cp lifish $DIR
+declare -A appimagetool_hashes
+appimagetool_hashes["aarch64"]="334e77beb67fc1e71856c29d5f3f324ca77b0fde7a840fdd14bd3b88c25c341f"
+appimagetool_hashes["x86_64"]="df3baf5ca5facbecfc2f3fa6713c29ab9cefa8fd8c1eac5d283b79cab33e4acb"
 
-pushd "$DIR"
+# Shared libraries as of Ubuntu 20.04
+required_libs=(
+	libsfml-audio.so.2.5
+	libsfml-graphics.so.2.5
+	libsfml-system.so.2.5
+	libsfml-window.so.2.5
 
-cat > run.sh <<'EOF'
-#!/bin/bash
+	libFLAC.so.8
+	libfreetype.so.6
+	libogg.so.0
+	libopenal.so.1
+	libsndio.so.7.0
+	libvorbis.so.0
+	libvorbisenc.so.2
+	libvorbisfile.so.3
+)
 
-root="$(dirname $(readlink -f $0))"
-[[ -x "$root/lifish" ]] || chmod +x "$root/lifish"
-LD_LIBRARY_PATH="$root/libs:$LD_LIBRARY_PATH" "$root/lifish"
-EOF
+cmake -S . -B build -DRELEASE=true
+cmake --build build -j "$(nproc)"
 
-chmod +x run.sh
+mkdir -p lifish.AppDir/usr/{bin,lib,share/applications}
 
-cat > README.txt <<'EOF'
-------------------------------------
-BOOM Remake by silverweed
-https://github.com/silverweed/lifish
-------------------------------------
+cp -a linux/* lifish.AppDir/
+cp -a assets/ lifish.AppDir/
+cp -a build/lifish lifish.AppDir/usr/bin/
 
-To run the game:
-
-- open a terminal
-- ./run.sh
-EOF
-
-mkdir libs
-ldd lifish | awk 'NF > 2 { print $3 }' | while read LIB; do
-	cp $LIB libs/
+for lib in "${required_libs[@]}"; do
+	cp -a -L "/lib/$system_arch-linux-gnu/$lib" lifish.AppDir/usr/lib/
 done
 
-popd
+if [ ! -f "$appimagetool_filename" ]; then
+	curl -sSf -L -O "$appimagetool_url"
+	echo "${appimagetool_hashes[$system_arch]}  $appimagetool_filename" | shasum -a 256 -c
+	chmod +x "$appimagetool_filename"
+fi
 
-echo "Created app in $DIR."
-echo Compressing...
+./"$appimagetool_filename" lifish.AppDir "BOOM-Remake-$sw_version-linux-$system_arch.AppImage"
 
-rm -f "$DIR.tar.xz" 2>/dev/null
-tar cf "$DIR.tar" "$DIR"
-xz "$DIR.tar"
-
-rm -rf "$DIR"
-echo Done.
+rm -r lifish.AppDir
