d81021c84f4d8f8b4cb1af95162759c579604dda
silverweed <silverweed14@proton.me>
2026-08-27 12:27:39 +0200

    remove non-boom bullets from conf

--- src/lifish/conf/bullet.cpp
+++ src/lifish/conf/bullet.cpp
@@ -20,19 +20,7 @@ std::map<unsigned, const lif::BulletData> lif::conf::bullet::data = {
 	{ 6, { "plasma.png", 12, 1, 4, 1 } },
 	// magma
 	{ 7, { "magma.png", 26, 1, 4, 4 } },
-	// rocket
-	{ 8, { "bullet8.png", /*TODO*/32, 2, 4, 4 } },
-	// grenade
-	{ 9, { "bullet9.png", 32, 1, 1, 1 } },
-	// rocket
-	{ 10, { "bullet10.png", 7, 1, 4, 1 } },
 	//// FreeBullets ////
 	// star
 	{ 101, { "bossbullet.png", 20, 2, 5, 0 } },
-	// spirit bullets
-	{ 102, { "spirit_bullets.png", 16, 2, 3, 0 } },
-	// missile
-	{ 103, { "missile.png", 32, 4, 4, 1 } },
-	// lightning
-	{ 104, { "lightning_bullet.png", 32, 4, 4, 1 } },
 };
